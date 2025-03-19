4bfa83788593422fa8667416a89ccd88c35dd279
Ashwini <ashwiniavhad8@gmail.com>
2025-03-19 17:38:19 +0530

    first commit

---
+++ src/main/java/com/example/designbookmyshowjava/DesignBookMyShowJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.designbookmyshowjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignBookMyShowJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignBookMyShowJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/designbookmyshowjava/DesignBookMyShowJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designbookmyshowjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignBookMyShowJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
